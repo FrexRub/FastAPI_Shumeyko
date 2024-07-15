f67f0db00f771fc776913d83a08fe76e96e8d258
SRubtsov <frexrub@gmail.com>
2024-07-15 11:35:12 +0300

    add docker compose progect

---
+++ docker/celery.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd src
+
+if [[ "${1}" == "celery" ]]; then
+  celery --app=tasks.tasks:celery worker -l INFO
+elif [[ "${1}" == "flower" ]]; then
+  celery --app=tasks.tasks:celery flower
+ fi
+
+## скрипт для заруска по параметрам celery или flower
